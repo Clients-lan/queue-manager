500e2d2de22132589ec92e6912aebc91c77b44b9
anthonylan <anthonylannn@gmail.com>
2020-07-20 18:59:29 +0200

    h

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const http = require('http')
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const app = express();
-//const server = http.createServer(app)
-//const io = socketio(server)
+const server = http.createServer(app)
+const io = socketio(server)
 //Passport config
 require('./config/passport')(passport);
 
@@ -25,20 +25,20 @@ const Booking = require('./modules/Booking');
 
 const { ensureAuthenticated } = require('./config/auth');
 
-//io.set('transports', ['websocket']);
+io.set('transports', ['websocket']);
 
 
 //@Run When clients connects
-// io.on('connection', socket => {
-//   socket.on('emiting', (data) => {
-//     socket.emit('emiting', data)
-//     socket.broadcast.emit('emiting', data)
-//   })
-//   socket.on('appt', (user) => {
-//     socket.emit('appt', user)
-//     socket.broadcast.emit('appt', user)
-//   })
-// })
+io.on('connection', socket => {
+  socket.on('emiting', (data) => {
+    socket.emit('emiting', data)
+    socket.broadcast.emit('emiting', data)
+  })
+  socket.on('appt', (user) => {
+    socket.emit('appt', user)
+    socket.broadcast.emit('appt', user)
+  })
+})
 
 //@EJS Template Engine
 app.use(expressLayouts);
@@ -221,45 +221,45 @@ app.post('/check-appt-client', (req, res) => {
 
 
 
-require('sticky-cluster')(
+// require('sticky-cluster')(
 
-  // server initialization function
-  function (callback) {
-   // var http = require('http');
-    //var app = require('express')();
-    const server = http.createServer(app);
-    const io = socketio(server)
-    io.on('connection', socket => {
-      socket.on('emiting', (data) => {
-        socket.emit('emiting', data)
-        socket.broadcast.emit('emiting', data)
-      })
-      socket.on('appt', (user) => {
-        socket.emit('appt', user)
-        socket.broadcast.emit('appt', user)
-      })
-    })
+//   // server initialization function
+//   function (callback) {
+//    // var http = require('http');
+//     //var app = require('express')();
+//     const server = http.createServer(app);
+//     const io = socketio(server)
+//     io.on('connection', socket => {
+//       socket.on('emiting', (data) => {
+//         socket.emit('emiting', data)
+//         socket.broadcast.emit('emiting', data)
+//       })
+//       socket.on('appt', (user) => {
+//         socket.emit('appt', user)
+//         socket.broadcast.emit('appt', user)
+//       })
+//     })
       
-    // configure an app
-      // do some async stuff if needed
+//     // configure an app
+//       // do some async stuff if needed
       
-    // don't do server.listen(), just pass the server instance into the callback
-    callback(server);
-  },
+//     // don't do server.listen(), just pass the server instance into the callback
+//     callback(server);
+//   },
   
-  // options
-  {
-    concurrency: 10,
-    port: PORT,
-    debug: true,
-    env: function (index) { return { stickycluster_worker_index: index }; }
-  }
-);
+//   // options
+//   {
+//     concurrency: 10,
+//     port: PORT,
+//     debug: true,
+//     env: function (index) { return { stickycluster_worker_index: index }; }
+//   }
+// );
 
 
 
 
 // listener
-// server.listen(PORT, function () {
-//     console.log(`listening on ${PORT}`);
-// });
+server.listen(PORT, function () {
+    console.log(`listening on ${PORT}`);
+});
