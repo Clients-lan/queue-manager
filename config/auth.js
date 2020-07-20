f6dde95584fc31c1657d6308055c79926940ea8c
anthonylan <anthonylannn@gmail.com>
2020-07-20 18:48:06 +0200

    h

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,8 @@ module.exports = {
     ensureAuthenticated: function(req, res, next) {
         if (req.isAuthenticated()) {
           return next();
+        } else {
+          return next();
         }
         req.flash('error_msg', 'Please log in to view that resource');
         res.redirect('/u/login');
